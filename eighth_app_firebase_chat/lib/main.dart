a76c473ae3b364cf251d15295be19bf545f5b1b9
robertrh3 <robertrh@Roberts-MacBook-Pro.local>
2020-05-12 16:37:59 -0600

    auth-screen-form-theme

--- eighth_app_firebase_chat/lib/main.dart
+++ eighth_app_firebase_chat/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './screens/chat_screen.dart';
+import './screens/auth_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,10 +12,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Chat',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        backgroundColor: Colors.pink,
+        accentColor: Colors.deepPurple,
+        accentColorBrightness: Brightness.dark,
+        buttonTheme: ButtonTheme.of(context).copyWith(
+          buttonColor: Colors.pink,
+          textTheme: ButtonTextTheme.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: ChatScreen(),
+      home: AuthScreen(),
+      // ChatScreen(),
     );
   }
 }
