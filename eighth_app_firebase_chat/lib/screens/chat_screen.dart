d00902000367c1a99df5687d96e49a2211434544
robertrh3 <robertrh@Roberts-MacBook-Pro.local>
2020-05-15 14:10:07 -0600

    firebase_chat_messaging

--- eighth_app_firebase_chat/lib/screens/chat_screen.dart
+++ eighth_app_firebase_chat/lib/screens/chat_screen.dart
@@ -2,11 +2,35 @@ import 'package:flutter/material.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
 // import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
 
 import '../widgets/chat/messages.dart';
 import '../widgets/chat/new_message.dart';
 
 class ChatScreen extends StatelessWidget {
+  // @override
+  // _ChatScreenState createState() => _ChatScreenState();
+// }
+
+// iOS push notification permission required prior to firebase notification messages
+// class _ChatScreenState extends State<ChatScreen> {
+  // @override
+  // void initState() {
+  //   final fbm = FirebaseMessaging();
+  //   fbm.requestNotificationPermissions();
+  //   fbm.configure(onMessage: (msg) {
+  //     print(msg);
+  //     return;
+  //   }, onLaunch: (msg) {
+  //     print(msg);
+  //     return;
+  //   }, onResume: (msg) {
+  //     print(msg);
+  //     return;
+  //   });
+  //   super.initState();
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
