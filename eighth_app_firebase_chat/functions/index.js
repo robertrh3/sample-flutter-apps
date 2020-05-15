db498d78d9464a01f5e6f5d3f1ce9b2bd561040b
robertrh3 <robertrh@Roberts-MacBook-Pro.local>
2020-05-15 15:08:21 -0600

    cloud-functions-javascript-execution

--- eighth_app_firebase_chat/functions/index.js
+++ eighth_app_firebase_chat/functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +9,14 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+exports.myFunction = functions.firestore
+    .document('chats/{chatId}/messages/{message}')
+    .onCreate((snapshot, context) => {
+        return admin.messaging().sendToTopic('chats', {
+            notification: {
+                title: snapshot.data().username,
+                body: snapshot.data().text,
+                clickAction: 'FLUTTER_NOTIFICATION_CLICK',
+            },
+        });
+    });
--- eighth_app_firebase_chat/lib/screens/chat_screen.dart
+++ eighth_app_firebase_chat/lib/screens/chat_screen.dart
@@ -2,34 +2,39 @@ import 'package:flutter/material.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
 // import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import '../widgets/chat/messages.dart';
 import '../widgets/chat/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
-  // @override
-  // _ChatScreenState createState() => _ChatScreenState();
-// }
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
 
 // iOS push notification permission required prior to firebase notification messages
-// class _ChatScreenState extends State<ChatScreen> {
-  // @override
-  // void initState() {
-  //   final fbm = FirebaseMessaging();
-  //   fbm.requestNotificationPermissions();
-  //   fbm.configure(onMessage: (msg) {
-  //     print(msg);
-  //     return;
-  //   }, onLaunch: (msg) {
-  //     print(msg);
-  //     return;
-  //   }, onResume: (msg) {
-  //     print(msg);
-  //     return;
-  //   });
-  //   super.initState();
-  // }
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    super.initState();
+    final fbm = FirebaseMessaging();
+    fbm.requestNotificationPermissions();
+    fbm.configure(onMessage: (msg) {
+      print(msg);
+      return;
+    }, onLaunch: (msg) {
+      print(msg);
+      return;
+    }, onResume: (msg) {
+      print(msg);
+      return;
+    });
+    // get device token - send notification to a specific device (i.e. two-person chat sends push notification for each participant)
+    // fbm.getToken();
+    //
+    // subscribe to a specific topic - subscribed notifications
+    fbm.subscribeToTopic('chats');
+  }
 
   @override
   Widget build(BuildContext context) {
