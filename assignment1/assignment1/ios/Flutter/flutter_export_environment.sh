5fe346ddb1f41bed8a182d0d6a7f42d4b4a64204
Robert Herrera <divrsfy@DiVrSFy-Mac-mini.local>
2020-06-25 14:17:05 -0600

    dart_2.7_min_sdk_updates_andcodechanges

--- assignment1/assignment1/ios/Flutter/flutter_export_environment.sh
+++ assignment1/assignment1/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/divrsfy/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/divrsfy/Development/udemy/sample-flutter-apps/second_app_trivia"
+export "FLUTTER_APPLICATION_PATH=/Users/divrsfy/Development/udemy/sample-flutter-apps/assignment1/assignment1"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
--- fifth_app_shop/lib/helpers/custom_route.dart
+++ fifth_app_shop/lib/helpers/custom_route.dart
@@ -15,7 +15,7 @@ class CustomRoute<T> extends MaterialPageRoute<T> {
     Animation<double> secondaryAnimation,
     Widget child,
   ) {
-    if (settings.isInitialRoute) {
+    if (settings.name == '/') {
       return child;
     }
     return FadeTransition(
@@ -34,7 +34,7 @@ class CustomPageTransitionBuilder extends PageTransitionsBuilder {
     Animation<double> secondaryAnimation,
     Widget child,
   ) {
-    if (route.settings.isInitialRoute) {
+    if (route.settings.name == '/') {
       return child;
     }
     return FadeTransition(
